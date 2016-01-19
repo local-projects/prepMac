8908dbcfa6ba8fddbc8f3c28d726a135ac5ce2e5
ericrieper <ericrieper@localprojects.net>
2016-01-19 18:13:22 -0500

    Includes screen saver, optional Bluetooth and Screen sharing, more robust existing items, temporarily removed dock cleanup, more verbose and color coded output

--- README.md
+++ README.md
@@ -2,15 +2,20 @@
 
 A simple bash script that takes care of the following things for preparing a machine for exhibition use:
 
-- Completely empties Dock then adds Safari, System Preferences, and Terminal shortcuts.
 - Disables "Crash Reporting" dialog.
 - Disables "Reopen Windows?" shutdown dialog.
 - Disables application state restoration dialog.
 - Disables Software Update from AppStore.
 - Disables sleep.
+- Disables screen saver.
 - Disables GateKeeper ("unauthorized developer" access.)
 - Disables Notification Center
 - Enables restart on power failure.
+
+And offers the options to:
+
+- Disable Bluetooth
+- Enable Screen Sharing
 
 ![Screenshot](https://raw.githubusercontent.com/local-projects/prepMac/screenshots/screen01.png)
 
@@ -21,9 +26,4 @@ Just run in Terminal with one line by downloading remotely with curl:
 	
 Or run normally after cloning or downloading with `sudo bash prepMac.sh`.
 
-The script prints it's actions to the console and each step may provide relevent feedback depending on your current system configuration (EG: If Notification Center has already been disabled and removed it will say it cannot find it for removal.)
-
-## Customizing
-Your software can be added to the Dock by adding the following in the dock section before `killall Dock` and replacing the YOUR APP.app string:
-	
-	defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/YOUR APP.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+The script prints its actions to the console and each step may provide relevent feedback depending on your current system configuration (EG: If Notification Center has already been disabled and removed it will say it cannot find it for removal.)
--- prepMac.sh
+++ prepMac.sh
@@ -1,71 +1,10 @@
 #!/bin/bash
 now="$(date +"%r")"
 
-printf "\n\n--- BEGINNING MACHINE PREP - $now ---\n\n"
-
-### Dock
-printf "\n Cleanup extra Dock icons? [Y / N]\n"
-read should_clean_dock
-
-if [$should_clean_dock == 'Y'] || [$should_clean_dock == 'y'] || [$should_clean_dock == 'yes'] || [$should_clean_dock == 'YES']
-then
-	printf "\n- Emptying, populating, and restarting Dock...\n"
-	# Remove all dock items
-	defaults write com.apple.dock persistent-apps -array ''
-	defaults write com.apple.dock persistent-others -array ''
-	# Add defaults: Safari, System Preferences, and Terminal
-	defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Safari.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
-	defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/System Preferences.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
-	defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Utilities/Terminal.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
-	### Add any other project apps here ###
-	killall Dock
-else
-	printf "\n- Skipping Dock cleanup.\n"
-fi
-
-### Bluetooth
-printf "\n Disable Bluetooth? [Y / N]\n"
-read should_disable_bluetooth
-
-if [$should_disable_bluetooth == 'Y'] || [$should_disable_bluetooth == 'y'] || [$should_disable_bluetooth == 'yes'] || [$should_disable_bluetooth == 'YES']
-then
-	printf "\n- Disabling Bluetooth...\n"
-	# Set bluetooth pref to off
-	defaults write /Library/Preferences/com.apple.Bluetooth.plist ControllerPowerState 0
-	# Kill the bluetooth server process	
-	killall blued
-	# Unload the daemon
-	launchctl unload /System/Library/LaunchDaemons/com.apple.blued.plist
-else
-	printf "\n- Skipping Bluetooth changes.\n"
-fi
-
-### Screen Sharing - NOTE: allows access for all users
-printf "\n Enable Screen Sharing via Remote Management? [Y / N]\n"
-read should_enable_screen_sharing
-
-if [$should_enable_screen_sharing == 'Y'] || [$should_enable_screen_sharing == 'y'] || [$should_enable_screen_sharing == 'yes'] || [$should_enable_screen_sharing == 'YES']
-then
-printf "\n- Enabling Screen Sharing...\n"
-	sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -off -restart -agent -privs -all -allowAccessFor -allUsers
-else
-	printf "\n- Skipping Screen Sharing.\n"
-fi
-
-### Enable Auto Login
-printf "\n Enable AUTO LOGIN for the user $USER ? [Y / N]\n"
-read should_enable_auto_login
-
-if [$should_enable_auto_login == 'Y'] || [$should_enable_auto_login == 'y'] || [$should_enable_auto_login == 'yes'] || [$should_enable_auto_login == 'YES']
-then
-printf "\n- Enabling auto login...\n"
-	sudo defaults write /Library/Preferences/com.apple.loginwindow "autoLoginUser" $USER
-else
-	printf "\n- Skipping auto login.\n"
-fi
+printf "\n\n\e[1m\e[7m --- BEGINNING MACHINE PREP - $now --- \e[0m\n\n"
 
 ### Crash Reporting
-printf "\n- Unloading and disabling crash reporting...\n"
+printf "\n\e[36m\e[4mUnloading and disabling crash reporting...\e[0m\n\e[2m"
 # Disable the dialogue from opening
 defaults write com.apple.CrashReporter DialogType none
 # Fully disable the diagnostic reporter?
@@ -73,41 +12,77 @@ launchctl unload -w /System/Library/LaunchDaemons/com.apple.DiagnosticReportClea
 sudo chmod 000 /System/Library/CoreServices/Problem\ Reporter.app
 
 ### "Repoen Windows" dialog
-printf "\n- Disabling \"reopen windows?\" dialog...\n"
+printf "\n\e[0m\e[36m\e[4mDisabling\"reopen windows?\" dialog...\e[0m\n\e[2m"
 defaults write com.apple.loginwindow LoginwindowLaunchesRelaunchApps -bool false
 defaults write com.apple.loginwindow TALLogoutSavesState -bool false
 
 ### Aplication state restoration dialog
-printf "\n- Disabling restore application state on crash, globally...\n"
+printf "\n\e[0m\e[36m\e[4mDisabling restore application state on crash, globally...\e[0m\n\e[2m"
 defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
 
 ### Software Update
-printf "\n- Disabling software updates...\n"
+printf "\n\e[0m\e[36m\e[4mDisabling software updates...\e[0m\n\e[2m"
 defaults write /Library/Preferences/com.apple.commerce AutoUpdate -bool false
 defaults write /Library/Preferences/com.apple.commerce AutoUpdateRestartRequired -bool false
 defaults write /Library/Preferences/com.apple.SoftwareUpdate AutomaticCheckEnabled -bool false
 sudo softwareupdate --schedule off
 
 ### Sleep
-printf "\n- Disabling sleep...\n"
+printf "\n\e[0m\e[36m\e[4mDisabling sleep...\e[0m \n\e[2m"
 sudo -S systemsetup -setsleep off
 
 ### Screen Saver
-printf "\n- Disabling screen saver... \n"
+printf "\n\e[0m\e[36m\e[4mDisabling screen saver...\e[0m \n\e[2m"
 defaults -currentHost write com.apple.screensaver idleTime 0
 
 ### GateKeeper
-printf "\n- Disabling GateKeeper...\n"
+printf "\n\e[0m\e[36m\e[4mDisabling GateKeeper...\e[0m\n\e[2m"
 spctl --master-disable
 
 ### Notification Center
-printf "\n- Unloading and disabling notification center...\n"
+printf "\n\e[0m\e[36m\e[4mUnloading and disabling notification center...\e[0m\n\e[2m"
 launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist
 killall NotificationCenter
 
 ### Restart on Power Failure
-printf "\n- Enabling restart on power failure...\n"
+printf "\n\e[0m\e[36m\e[4mEnabling restart on power failure...\e[0m\n\e[2m"
 systemsetup -setrestartpowerfailure on
+printf "\e[0m"
+
+### Bluetooth
+echo
+printf "\e[0m"
+read -n 1 -p "Disable Bluetooth? [y / n]: " should_disable_bluetooth
+echo
+if [ "$should_disable_bluetooth" = "y" ] || [ "$should_disable_bluetooth" = "Y" ]
+	then
+		printf "\n\e[0m\e[36m\e[4mDisabling Bluetooth...\e[0m\n\e[2m"
+		# Set bluetooth pref to off
+		defaults write /Library/Preferences/com.apple.Bluetooth.plist ControllerPowerState 0
+		# Kill the bluetooth server process	
+		killall blued
+		# Unload the daemon
+		launchctl unload /System/Library/LaunchDaemons/com.apple.blued.plist
+	else
+		printf "\n- Skipping Bluetooth changes.\n"
+fi
+
+### Screen Sharing - NOTE: allows access for all users
+echo
+printf "\e[0m"
+read -n 1 -p "Enable Screen Sharing via Remote Management? [y / n]: " should_enable_screen_sharing
+echo
+if [ "$should_enable_screen_sharing" = "y" ] || [ "$should_enable_screen_sharing" = "Y" ]
+	then
+		printf "\n\e[0m\e[36m\e[4mEnabling Screen Sharing...\e[0m\n\e[2m"
+		sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -off -restart -agent -privs -all -allowAccessFor -allUsers
+	else
+		printf "\n- Skipping Screen Sharing.\n"
+fi
+
+### Enable Auto Login
+echo
+printf "\e[0mTo enable AUTO LOGIN please use System Preferences\n"
 
 now="$(date +"%r")"
-printf "\n--- COMPLETED MACHINE PREP - $now ---\n\n"
+printf "\n\e[0m\e[1m\e[5m\e[7m --- COMPLETED MACHINE PREP - $now --- \e[0m\n\n"
