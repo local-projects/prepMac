33fdb0382f028d3beb221aa384c13e38f2c40220
ericrieper <ericrieper@localprojects.net>
2016-05-17 15:38:30 -0400

    Added brute force prevention of state restoration, fixed typo in pmset restart on freeze command, re-organized

--- prepMac.sh
+++ prepMac.sh
@@ -53,13 +53,17 @@ if [ $# -ne 0 ]
 		fi
 fi
 
+### Restart on Power Failure ###################################################
+style "header" "Enabling restart on power failure..."
+sudo systemsetup -setrestartpowerfailure on
+
 ### Restart on computer freeze #################################################
 style "header" "Enabling automatic restart on computer freeze..."
 sudo systemsetup -setrestartfreeze on # Both methods here do the same thing.
-sudo pmset autoreset 1
+sudo pmset autorestart 1
 
 ### Crash Reporting ############################################################
-style "header" "Unloading and disabling crash reporting..."
+style "header" "Unloading and disabling crash reporting (brute force chmod)..."
 # Disable the dialogue from opening
 defaults write com.apple.CrashReporter DialogType none
 # Fully disable the diagnostic reporter?
@@ -76,6 +80,10 @@ style "header" "Disabling restore application state on crash, globally..."
 defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
 defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -boolean false # Seems to need "-boolean" ?
 
+style "header" "Disabling application states completely (brute force chmod)..."
+rm -r ~/Library/Saved\ Application\ State/*
+chmod -R a-w ~/Library/Saved\ Application\ State/
+
 ### Software Update ############################################################
 style "header" "Disabling software updates..."
 sudo defaults write /Library/Preferences/com.apple.commerce AutoUpdate -bool false
@@ -88,6 +96,10 @@ style "header" "Disabling sleep..."
 sudo systemsetup -setsleep off
 sudo systemsetup -setcomputersleep Off > /dev/null
 
+### GateKeeper #################################################################
+style "header" "Disabling GateKeeper..."
+sudo spctl --master-disable
+
 ### Screen Saver ###############################################################
 style "header" "Disabling screen saver..."
 rm ~/Library/Preferences/ByHost/com.apple.screensaver.*
@@ -96,34 +108,26 @@ defaults -currentHost delete com.apple.screensaver
 defaults -currentHost write com.apple.screensaver idleTime 0
 sudo defaults write /Library/Preferences/com.apple.screensaver loginWindowIdleTime 0
 
-### TextEdit defaults ##########################################################
-style "header" "Making TextEdit default to PlainText with UTF-8 encoding..."
-defaults write com.apple.TextEdit RichText -int 0
-defaults write com.apple.TextEdit PlainTextEncoding -int 4
-defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
+### Notification Center ########################################################
+style "header" "Unloading and disabling notification center..."
+launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
+killall NotificationCenter
+
+### Bluetooth auto find keyboard ###############################################
+style "header" "Disabling the auto Bluetooth Setup Assistant window..."
+sudo defaults write /Library/Preferences/com.apple.Bluetooth BluetoothAutoSeekKeyboard '0'
+sudo defaults write /Library/Preferences/com.apple.Bluetooth BluetoothAutoSeekPointingDevice '0'
 
 ### Desktop wallpaper ##########################################################
 style "header" "Removing desktop image..."
 osascript -e 'tell application "System Events" to set picture of every desktop to ("/Library/Desktop Pictures/Solid Colors/Solid Gray Pro Ultra Dark.png" as POSIX file as alias)'
 sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '/Library/Desktop Pictures/Solid Colors/Solid Gray Pro Ultra Dark.png'"
 
-### GateKeeper #################################################################
-style "header" "Disabling GateKeeper..."
-sudo spctl --master-disable
-
-### Notification Center ########################################################
-style "header" "Unloading and disabling notification center..."
-launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
-killall NotificationCenter
-
-### Restart on Power Failure ###################################################
-style "header" "Enabling restart on power failure..."
-sudo systemsetup -setrestartpowerfailure on
-
-### Bluetooth auto find keyboard ###############################################
-style "header" "Disabling the auto Bluetooth Setup Assistant window..."
-sudo defaults write /Library/Preferences/com.apple.Bluetooth BluetoothAutoSeekKeyboard '0'
-sudo defaults write /Library/Preferences/com.apple.Bluetooth BluetoothAutoSeekPointingDevice '0'
+### TextEdit defaults ##########################################################
+style "header" "Making TextEdit default to PlainText with UTF-8 encoding..."
+defaults write com.apple.TextEdit RichText -int 0
+defaults write com.apple.TextEdit PlainTextEncoding -int 4
+defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
 
 ### Daily 4:00 AM Restart #####################################################
 if [ $should_default = false ]
